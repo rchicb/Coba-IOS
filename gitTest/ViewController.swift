9d79bd3c051f5d8855c11be491a7585e48d17b41
BCS Media <bcs@bcss-imac.local>
2016-12-29 16:29:32 -0600

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by BCS Media on 12/29/16.
+//  Copyright © 2016 BCS Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
